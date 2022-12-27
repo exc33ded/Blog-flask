f13d718e42bb2e215ad89733c67961265ca4a368
Mohammed Sarim <sarimisraiely12@gmail.com>
2022-12-27 18:11:43 +0530

    git commit

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# Create Flask Instance
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return "<h1>Hello, World!</h1>"
 
+@app.route("/user/<name>")
+def user(name):
+    return f"<h2>Hello {name}</h2>"
+
+
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
