a00f7ad5e56f342a2aa4dfe9b994a07fe15b72f5
Mohammed Sarim <sarimisraiely12@gmail.com>
2022-12-27 23:48:27 +0530

    commit

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 # Create Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "qwerty2212"
+
+# Create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your Name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route("/")
 def index():
@@ -13,7 +23,7 @@ def user(name):
     return render_template("user.html", user_name=name, pizzatopping=fav_pizza)
 
 
-# Create Custom Errpr pages
+# Create Custom Error pages
 
 # Invalid URL
 @app.errorhandler(404)
@@ -23,6 +33,17 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create Name Page
+@app.route("/name", methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name=form.name.data
+        form.name.data = ''
+    return render_template("name.html",name=name, form=form)
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", debug=True)
